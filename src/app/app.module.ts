6829ac3f42f11524bed004081aea6c2b77bf145b
jcguerrero21 <jotaceguerrerom21@gmail.com>
2019-01-08 11:28:14 +0100

    explicación básica virtual scroll y dragDrop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VirtualComponent } from './virtual/virtual.component';
+
+import { ScrollingModule } from '@angular/cdk/scrolling'; 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
+import { DragComponent } from './drag/drag.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { PaisesComponent } from './paises/paises.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VirtualComponent,
+    DragComponent,
+    PaisesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScrollingModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/paises/paises.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-paises',
+  templateUrl: './paises.component.html',
+  styleUrls: ['./paises.component.css']
+})
+export class PaisesComponent implements OnInit {
+
+  paises: any = [];
+
+  constructor(private _http: HttpClient) { }
+
+  ngOnInit() {
+    this._http.get(`https://restcountries.eu/rest/v2/lang/es`).subscribe(
+      paises => {
+        this.paises = paises;
+      }
+    )
+  }
+
+  drop(event: CdkDragDrop<any> ){
+    //console.log('OK', event);
+    moveItemInArray( this.paises, event.previousIndex, event.currentIndex);
+  }
+
+}
